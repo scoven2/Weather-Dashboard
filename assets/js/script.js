31bc8a3c8b359dbd6b10462f0314cb537af38d91
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-23 17:29:27 -0500

    final functions in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,4 +56,79 @@ $(document).ready(function() {
       "<h2>" + data.weather[0].main + "</h2>" + "<img src=" + imgApi + data.weather[0].icon + ".png alt=" + data.weather.value + 'width="50" height="50"></img>' + '<p class="humidity"> Humidity: ' + data.main.humidity + "%</p>" + '<p class="temperature">Temperature: ' + Math.trunc(data.main.temp_max) + "&deg;F</p>" + '<p class="wind-speed">Wind Speed: ' + data.wind.speed + " m/h</p>"
     )
   }
+  function UVIndex(data) {
+    $.ajax({
+      url: UV + appID + "@lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&cnt=" + 5,
+      type: "GET",
+    }).then(function(data) {
+      var color = UVColor(data.value)
+      currentDay.append('<p class="uv">UV Index: ' + data.value + "</p>");
+      $(".uv").css("color", color)
+    });
+  }
+  function UVColor(UV) {
+    if (UV <= 2) {
+      return "green"
+    } else if (UV <= 5) {
+      return "yellow"
+    } else if (UV <= 7) {
+      return "orange"
+    } else if (UV <= 10) {
+      return "red"
+    } else {
+      return "purple"
+    }
+  }
+  function getFiveDay(city){
+    if (city !== "") {
+      $.ajax({
+        url: queryFiveDay + city + "&units=imperial" + appID,
+        type: "GET",
+        dataType: "jsonp",
+      }).then(function(data) {
+        var showFiveDays = showFiveDay(data);
+        $("#forcast .day").append(showFiveDays);
+      });
+    } else {
+      $("error").html("Field must be filled");
+    }
+  }
+  function showFiveDay(data) {
+    var cardForecast =$("#cardsForcast")
+    cardForecast.html("");
+    for (let i = 0; i < 5; i++) {
+      var new_date = moment().add(i, "days").format("dddd");
+      let forcast = function(data) {
+        return (
+          '<div class="col-sm-12 col-md-6 col-lg-2">' + '<p class"date"></p>' + new_date + "<img src=" + imgAPI + data.list[i * 8].weather[0].icon + ".png alt=" + data.list[i * 8].weather.description + 'width="50" height="50">' + '<p class="temperature">Temp: ' + Math.trunc(data.list[i * 8].main.temp_max) + "&nbsp;&deg;F</p>" + '<p class="humidity">Humidity: ' + data.list[i * 8].main.humidity + "%</p>" + "</div>"
+        );
+      };
+      cardForecast.append(forcast(data));
+    }
+  }
+  $("#btn-search").click(function() {
+    var city = $("#input-city").val();
+    getCurrent(city);
+  });
+  function showCity(city) {
+    presentCities.append(
+      '<button type="button" class="list-group-item list-group-item-action">' + city + "</button>"
+    );
+  }
+  function save(city) {
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+  }
+  function isDuplicate(city) {
+    for (cityName of cities) {
+      if (cityName == city) {
+        return true;
+      }
+    }
+    return false;
+  }
+  presentCities.click(function (event) {
+    var city = event.target.textContent;
+    getCurrent(city);
+  });
 });
