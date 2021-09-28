f5711af09ef54d508e2d0a2cb96a0211445df2cc
Scott Siegel <scottmatthewsiegel@gmail.com>
2021-09-28 15:05:56 -0500

    added ability to fetch api info and sited class example as source of original work

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,11 +41,37 @@ let formSubmitHandler = function(event) {
   }
   savedSearch();
   pastSearch(city);
-}
+};
 
 //based on unit 4 activity 23
 //localStorage.setItem("studentGrade", JSON.stringify(studentGrade));
 //renderMessage();
 let saveSearch = function() {
   localStorage.setItem("cities", JSON.stringify(cities));
-}
+};
+
+//based on unit 6 activity 24
+// var getFeaturedRepos = function (language) {
+//     var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
+
+//     fetch(apiUrl).then(function (response) {
+//       if (response.ok) {
+//         response.json().then(function (data) {
+//           displayRepos(data.items, language);
+//         });
+//       } else {
+//         alert('Error: ' + response.statusText);
+//       }
+//     });
+//   };
+let getWeather = function(city) {
+  let apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=e8b45019544b39375a45ac056b5a30ee'
+  fetch(apiURL)
+    .then(function(response) {
+      response.json()
+        .then(function(data) {
+          displayWeather(data, city);
+        })
+    })
+};
+
